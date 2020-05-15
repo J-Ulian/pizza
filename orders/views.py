7027fa4fdbc1bd97da42d4c34fdb798667f2c627
J Ulian <hudatov@gmail.com>
2020-05-15 17:18:58 +0300

    index and admin

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Pizza
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    context = {
+        "menu": Pizza.objects.all()
+    }
+    return render(request, "orders/index.html", context)
+    #return HttpResponse("Project 3: TO DO")

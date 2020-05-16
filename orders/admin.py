d64c9de293ef3d1ea1a732ad2718258c89fbcc42
J Ulian <hudatov@gmail.com>
2020-05-16 16:27:48 +0300

    admin models

--- orders/admin.py
+++ orders/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 
 from .models import Pizza, Topping
 # Register your models here.
-admin.site.register(Pizza)
-admin.site.register(Topping)
+
+
+class ToppingAdmin(admin.ModelAdmin):
+    filter_horizontal = ("pizzas",)
+
+
+class ToppingInline(admin.StackedInline):
+    model = Topping.pizzas.through
+    extra = 1
+
+
+class PizzaAdmin(admin.ModelAdmin):
+    inlines = [ToppingInline]
+
+
+admin.site.register(Pizza, PizzaAdmin)
+admin.site.register(Topping, ToppingAdmin)
